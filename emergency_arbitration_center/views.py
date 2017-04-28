dbe005b7c898a46e75b7d221d87fd1125e4b0e52
Nilanjan <nilanjandaw@gmail.com>
2017-04-28 19:08:39 +0530

    arbitration server logic updated

--- emergency_arbitration_center/views.py
+++ emergency_arbitration_center/views.py
@@ -1,19 +1,24 @@
 import requests
+import datetime
+
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from django.contrib.gis.measure import D
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.db.models.functions import Distance
+from django.conf import settings
+from django.utils import timezone
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
 from emergency_arbitration_center.models import emergency
 from emergency_arbitration_center.serializer import EmergencySerializer
 from login_server.models import User
 from login_server.serializer import UserSerializer
 from personnel_login_server.models import EmergencyPersonnel
 from personnel_login_server.serializer import EmergencyPersonnelSerializer
-from django.conf import settings
-from rest_framework.renderers import JSONRenderer
 
 
 @api_view(['GET', 'POST'])
@@ -28,9 +33,10 @@ def getNotification(request):
         user_adhaar_number = request.data['user_adhaar_number']
         profile_data = User.objects.filter(adhaar_number=user_adhaar_number)
         if len(profile_data) > 0:
+            time_threshold = timezone.now() - datetime.timedelta(minutes=10)
             emergency_location = GEOSGeometry(request.data['location'])
             emergency_type = request.data['emergency_type']
-            assignment_list = EmergencyPersonnel.objects.filter(location__distance_lte=(emergency_location, D(m=2000)), \
+            assignment_list = EmergencyPersonnel.objects.filter(last_update__gte=time_threshold, location__distance_lte=(emergency_location, D(m=5000)), \
                 status=1, responder_type=emergency_type) \
                 .annotate(distance=Distance('location', emergency_location)) \
                 .order_by('distance')
